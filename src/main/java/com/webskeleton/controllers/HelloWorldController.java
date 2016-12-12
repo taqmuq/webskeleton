ccfba1b8d257e52477d11250b88778b73acc6a4f
taqmuq <taqmuq.enterprises@gmail.com>
2016-12-12 03:32:13 -0800

    Final commit

---
+++ src/main/java/com/webskeleton/controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.webskeleton.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by ramanyakkala on 12/12/16.
+ */
+@Controller
+public class HelloWorldController {
+    @RequestMapping
+    public String sayHello() {
+        return "index";
+    }
+}
