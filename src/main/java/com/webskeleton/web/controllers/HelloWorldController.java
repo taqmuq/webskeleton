f7f7467fd0e954f5c3fdff6ef5924da322dc0b93
taqmuq <taqmuq.enterprises@gmail.com>
2016-12-12 15:57:57 -0800

    Addred bootstrap and jquery dependencies to pom.xml

--- src/main/java/com/webskeleton/web/controllers/HelloWorldController.java
+++ src/main/java/com/webskeleton/web/controllers/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.webskeleton.controllers;
+package com.webskeleton.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
