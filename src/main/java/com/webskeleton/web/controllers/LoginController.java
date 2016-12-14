0c7d56a313f7ab94746fa30c41199d1f97fabd74
taqmuq <taqmuq.enterprises@gmail.com>
2016-12-13 17:44:10 -0800

    login related

---
+++ src/main/java/com/webskeleton/web/controllers/LoginController.java
@@ -0,0 +1,19 @@
+package com.webskeleton.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by taqmuq on 25/03/2016.
+ */
+@Controller
+public class LoginController {
+
+    /** The login view name */
+    public static final String LOGIN_VIEW_NAME = "user/login";
+
+    @RequestMapping("/login")
+    public String login() {
+        return LOGIN_VIEW_NAME;
+    }
+}
---
+++ src/main/java/com/webskeleton/web/controllers/PayloadController.java
@@ -0,0 +1,18 @@
+package com.webskeleton.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by taqmuq on 25/03/2016.
+ */
+@Controller
+public class PayloadController {
+
+    public static final String PAYLOAD_VIEW_NAME = "payload/payload";
+
+    @RequestMapping("/payload")
+    public String payload() {
+        return PAYLOAD_VIEW_NAME;
+    }
+}
