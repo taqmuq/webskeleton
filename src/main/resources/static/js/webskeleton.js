e0c19b35188cd82df34a437bc16b680b879f91dd
taqmuq <taqmuq.enterprises@gmail.com>
2016-12-15 23:20:12 -0800

    login form validation

--- src/main/resources/static/js/webskeleton.js
+++ src/main/resources/static/js/webskeleton.js
@@ -106,4 +106,30 @@ function main() {
             }
         }
     });
+
+    /* Login page form validation */
+    $('#loginForm').formValidation({
+        framework: 'bootstrap',
+        icon: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            username: {
+                validators: {
+                    notEmpty: {
+                        message: 'The username is required'
+                    }
+                }
+            },
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: 'The password is required'
+                    }
+                }
+            }
+        }
+    });
 }
